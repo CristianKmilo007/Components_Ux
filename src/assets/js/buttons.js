71564f812e43afdc152b17b0899848a1f19b3240
Cristian Rojas <front-end@todoservy.com>
2023-07-10 16:31:00 -0500

    add 3 buttons

--- src/assets/js/buttons.js
+++ src/assets/js/buttons.js
@@ -1,24 +1,25 @@
+
+/* ------------ BUTTON 8 ---------------*/
 const button8 = document.querySelectorAll('.button8-link')
 
-    button8.forEach(btn =>{
-        btn.addEventListener('mouseenter', function(e){
-            let x = e.offsetX
-            let y = e.offsetY
+button8.forEach(btn =>{
+    btn.addEventListener('mouseenter', function(e){
+        let x = e.offsetX
+        let y = e.offsetY
 
-            let ripples = document.createElement('span')
-            ripples.style.left = x + 'px'
-            ripples.style.top = y + 'px'
+        let ripples = document.createElement('span')
+        ripples.style.left = x + 'px'
+        ripples.style.top = y + 'px'
 
-            this.appendChild(ripples)
-            console.log(e);
+        this.appendChild(ripples)
+        console.log(e);
 
-            setTimeout(() => {
-                ripples.remove()
-            }, 1000)
-        })
+        setTimeout(() => {
+            ripples.remove()
+        }, 1000)
     })
+})
 
-    console.log(button8);
 
 
 
